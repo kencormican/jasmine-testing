38595361f63b69bf6f9af3cec2e7d8c20f414ec3
kencormican <ken.cormican@bt.com>
2020-06-27 15:47:11 +0000

    All drinkGame Specs and Scripts added and validated via Jasmine

--- scripts/drinkGame.js
+++ scripts/drinkGame.js
@@ -1,6 +1,21 @@
 function whatCanIDrink(age) {
-    if(age<0){
-        return "Error!"
+    if(age < 0){
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
     }
-
+    else if (0 < age && age < 14) {
+            return "Drink Toddy";
+        }
+    
+    else if (14 < age &&  age < 18) {
+            return "Drink Coke";
+        }
+    else if (18 < age &&  age < 21) {
+            return "Drink Beer";
+        }
+    else if (21 < age &&  age < 130) {
+            return "Drink Whiskey";
+        }
+    else {
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
 }
